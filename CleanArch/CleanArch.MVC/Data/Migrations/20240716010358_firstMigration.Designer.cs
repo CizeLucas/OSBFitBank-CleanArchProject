ef2fa303cb9434193ba0ac1d855b7efad706346b
CizeLBook2 <cizelucas@gmail.com>
2024-07-15 22:06:46 -0300

    finished Implementando Clean Arch I

--- CleanArch/CleanArch.MVC/Data/Migrations/20240716010358_firstMigration.Designer.cs
+++ CleanArch/CleanArch.MVC/Data/Migrations/20240716010358_firstMigration.Designer.cs
@@ -4,6 +4,7 @@ using CleanArch.MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArch.MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240716010358_firstMigration")]
+    partial class firstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
